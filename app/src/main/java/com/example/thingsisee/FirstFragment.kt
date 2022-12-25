b9aa4bcee91b0058c990e0a91de9bb4ac206efaf
karpukhinp01 <88552184+karpukhinp01@users.noreply.github.com>
2022-12-25 19:10:27 +0100

    List and Post, makes Toast at the right time delete works strange

--- app/src/main/java/com/example/thingsisee/FirstFragment.kt
+++ app/src/main/java/com/example/thingsisee/FirstFragment.kt
@@ -1,10 +1,6 @@
 package com.example.thingsisee
 
-import android.app.Activity
-import android.app.Application
-import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -13,12 +9,9 @@ import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import com.example.thingsi.Data.Post
 import com.example.thingsisee.ViewModels.MainViewModel
 import com.example.thingsisee.databinding.FragmentFirstBinding
-import java.util.*
-import kotlin.concurrent.schedule
-import kotlin.concurrent.timer
+
 
 class FirstFragment : Fragment() {
     private var _binding: FragmentFirstBinding? = null
