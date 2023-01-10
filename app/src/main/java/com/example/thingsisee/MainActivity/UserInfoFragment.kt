c6ef9b795b0b838eaef887131ebcf3b59121118f
karpukhinp01 <88552184+karpukhinp01@users.noreply.github.com>
2023-01-10 21:17:13 +0100

    post post and displays the message. Nice + backButton + hideKeyboard + updateProfilePic

--- app/src/main/java/com/example/thingsisee/MainActivity/UserInfoFragment.kt
+++ app/src/main/java/com/example/thingsisee/MainActivity/UserInfoFragment.kt
@@ -1,6 +1,7 @@
 package com.example.thingsisee.MainActivity
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -13,6 +14,8 @@ import com.example.thingsisee.R
 import com.example.thingsisee.ViewModels.MainViewModel
 import com.example.thingsisee.databinding.FragmentRegisterBinding
 import com.example.thingsisee.databinding.FragmentUserInfoBinding
+import com.google.firebase.auth.ktx.userProfileChangeRequest
+import com.google.firebase.storage.FirebaseStorage
 
 class UserInfoFragment : Fragment() {
     private var _binding: FragmentUserInfoBinding? = null
@@ -37,6 +40,16 @@ class UserInfoFragment : Fragment() {
                 mMainViewModel.resetStatus()
             }
         })
+
+        val storageRef = FirebaseStorage.getInstance().getReference("/profilePics/Screenshot 2021-05-05 164534.png")
+        storageRef.downloadUrl.addOnSuccessListener {
+            val profilePicUpdate = userProfileChangeRequest {
+                photoUri = it
+            }
+            mMainViewModel.user!!.updateProfile(profilePicUpdate).addOnSuccessListener {
+                mMainViewModel.updateUri()}
+        }
+
 
         binding.doneButton.setOnClickListener {
             val name = binding.enterName
